d8528db85c296372ec531c1515711b917f891ac3
smokingplaya <smokingplaya@yandex.ru>
2024-10-05 00:40:11 +0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -21,39 +21,45 @@ import {
   noctisViola
 } from "react-code-blocks";
 
-const githubUrl = "https://github.com/smokingplaya/"
+const links = {
+  github: "https://github.com/smokingplaya",
+  telegram: "https://t.me/smokingplaya"
+}
 
 function App() {
   return (
     <>
       <PageBackground/>
-      <main className="w-screen h-screen flex items-center justify-center">
+      <main className="w-screen h-screen flex items-center justify-center overflow-hidden">
         <Page>
-          {/* model */}
           <PageModel/>
-          {/* page content */}
           <PageContent>
             <PageContentTitle textUnderline={true}>smokingplaya</PageContentTitle>
             <PageContentText>16yo software engineer based in Russia 🇷🇺</PageContentText>
-            <PageContentSubtitle>Skills</PageContentSubtitle>
+            <PageContentSubtitle>The languages I use</PageContentSubtitle>
+
             <PageSkills>
-              <PageSkill>Rust</PageSkill>
+              <PageSkill>❤ Rust</PageSkill>
               <PageSkill>TypeScript</PageSkill>
               <PageSkill>Lua</PageSkill>
-              <PageContentText>You can see my other skills on my <PageLink href={githubUrl}><GithubIcon className="w-5"/> GitHub</PageLink></PageContentText>
+              <PageContentText>You can see my other skills on my <PageLink href={links.github}><GithubIcon className="w-5"/> GitHub</PageLink></PageContentText>
             </PageSkills>
 
-            <PageContentSubtitle>Try <PageLink className="text-stone-300" href={githubUrl + "uki"}>uki</PageLink>!</PageContentSubtitle>
+            <PageContentSubtitle>Try <PageLink className="text-violet-300" href={links.github + "uki"}>uki</PageLink>!</PageContentSubtitle>
             <PageContentText>Makefile-like utility based on YAML</PageContentText>
-            <CodeBlock language="bash" text={'/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/smokingplaya/uki/HEAD/misc/install.sh)"'} theme={noctisViola} showLineNumbers={true} />
+
+            <div className="w-72 md:w-96 xl:w-auto">
+              <CodeBlock language="bash" text={'/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/smokingplaya/uki/HEAD/misc/install.sh)"'} theme={noctisViola} showLineNumbers={true} />
+            </div>
 
             <PageSpaceBetween/>
 
             <PageFooter>
               <PageContentSubtitle>Other helpful links</PageContentSubtitle>
               <PageFooterLinks>
-                <PageFooterLink href={githubUrl}>GitHub</PageFooterLink>
-                <PageFooterLink href="https://contract.gosuslugi.ru/">Хочешь помочь родине? Запишись на конткрактную службу!</PageFooterLink>
+                <PageFooterLink href={links.github}>GitHub</PageFooterLink>
+                <PageFooterLink href={links.telegram}>Telegram</PageFooterLink>
+                <PageFooterLink href="https://contract.gosuslugi.ru/">Помочь родине 🇷🇺</PageFooterLink>
               </PageFooterLinks>
             </PageFooter>
           </PageContent>
--- src/components/page.tsx
+++ src/components/page.tsx
@@ -7,29 +7,29 @@ const backgroundUrl = "https://cdna.artstation.com/p/assets/images/images/033/69
 export function Page(
   {children}: {children?: ReactNode}
 ) {
-  return (
-    <div className="w-auto h-auto bg-stone-800 rounded-2xl flex" children={children}/>
-  )
+  return <div className="w-auto h-auto bg-stone-800 rounded-2xl flex" children={children}/>
 }
 
 export function PageBackground() {
   return (
     <div className="w-full h-full">
-      <img className="absolute inset-0 w-full h-full object-cover -z-10" src={backgroundUrl} />
+      <img className="absolute inset-0 w-full h-screen object-cover -z-10" src={backgroundUrl} />
       <div className="absolute inset-0 backdrop-blur-sm -z-10" />
     </div>
   )
 }
 
 function RenderModel() {
-  const { scene } = useGLTF('/model.gltf');
+  const { scene } = useGLTF('/treemodel/scene.gltf');
   return <primitive object={scene} />;
 }
 
 export function PageModel() {
   return (
-    <div className="flex flex-col w-full h-full">
-      <Canvas camera={{ position: [0, 0, 5], fov: 50 }} className="w-full h-full">
+    <div className="hidden md:w-96 md:flex flex-col">
+      <Canvas
+        camera={{position: [4, 0, -4.5], fov: 40 }}
+        className="w-full h-full">
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Suspense fallback={null}>
@@ -44,41 +44,31 @@ export function PageModel() {
 export function PageContent(
   {children}: {children?: ReactNode}
 ) {
-  return (
-    <div className="w-full h-full p-8 flex flex-col justify-between" children={children}/>
-  )
+  return <div className="w-full h-full p-8 flex flex-col justify-between" children={children}/>
 }
 
 export function PageContentTitle(
   {children, textUnderline}: {children?: ReactNode, textUnderline?: boolean}
 ) {
-  return (
-    <h1 className={"text-stone-300 font-bold text-4xl mb-4 " + (textUnderline ? " underline decoration-wavy decoration-2 underline-offset-6 decoration-violet-400" : "")}>{children}</h1>
-  )
-}
-
-export function PageContentText(
-  {children}: {children?: ReactNode}
-) {
-  return (
-    <p className="text-stone-400 font-medium text-xl">{children}</p>
-  )
+  return <h1 className={"text-stone-300 font-bold text-2xl md:text-4xl mb-4 " + (textUnderline ? " underline decoration-wavy decoration-2 underline-offset-6 decoration-violet-400" : "")}>{children}</h1>
 }
 
 export function PageContentSubtitle(
   {children}: {children?: ReactNode}
 ) {
-  return (
-    <h1 className="text-stone-300 font-bold mt-2 text-xl">{children}</h1>
-  )
+  return <h1 className="text-stone-300 font-bold mt-2 text-md md:text-xl" children={children}/>
+}
+
+export function PageContentText(
+  {children}: {children?: ReactNode}
+) {
+  return <p className="text-stone-400 font-medium text-md md:text-xl" children={children}/>
 }
 
 export function PageLink(
   {children, href, className}: {children?: ReactNode, href: string, className?: string}
 ) {
-  return (
-    <a className={"font-bold inline-flex transition items-center hover:text-violet-400 " + (className || "text-stone-500")} href={href}>{children}</a>
-  )
+  return <a target="_blank" className={"text-md md:text-xl font-bold inline-flex transition items-center hover:text-violet-400 " + (className || "text-stone-500")} href={href}>{children}</a>
 }
 
 /// Skills
@@ -86,17 +76,13 @@ export function PageLink(
 export function PageSkills(
   {children}: {children?: ReactNode}
 ) {
-  return (
-    <div>{children}</div>
-  )
+  return <ul className="list-inside list-disc" children={children}/>
 }
 
 export function PageSkill(
   {children}: {children?: ReactNode}
 ) {
-  return (
-    <div className="text-violet-200 font-bold text-m">{children}</div>
-  )
+  return <li className="text-violet-400 font-bold text-m" children={children}/>
 }
 
 /// Footer
@@ -104,33 +90,21 @@ export function PageSkill(
 export function PageFooter(
   {children}: {children?: ReactNode}
 ) {
-  return (
-    <div className="mt-auto">
-      {children}
-    </div>
-  )
+  return <div className="mt-auto" children={children}/>
 }
 
 export function PageFooterLink(
   {children, href}: {children?: ReactNode, href: string}
 ) {
-  return (
-    <PageLink href={href}>{children}</PageLink>
-  )
+  return <PageLink href={href} children={children}/>
 }
 
 export function PageFooterLinks(
   {children}: {children?: ReactNode}
 ) {
-  return (
-    <div className="flex gap-x-4">
-      {children}
-    </div>
-  )
+  return <div className="flex gap-x-2" children={children}/>
 }
 
 export function PageSpaceBetween() {
-  return (
-    <div className="mt-16"/>
-  )
+  return <div className="mt-4 md:mt-8"/>
 }
